876985eaa38822502d13480414891170bae32697
JonasRoo <jonas.roosen@outlook.com>
2020-09-05 17:14:25 +0200

    Fixed client secrets

--- backend/src/root/settings.py
+++ backend/src/root/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
+from .local_settings import *
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -20,7 +22,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'a2x7nt!6kxw83(9tccgyytm@*yy2+*dzs@%h+4=#1yrm)^)jjs'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
