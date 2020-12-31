49cea260758f453a1e014d3ab90cc7e24fe9edfa
JonasRoo <jonas-roosen@t-online.de>
2020-12-31 20:31:30 +0100

    refactored manual filters into "official" filters with django_filters backend

---
+++ backend/src/articles/api/filters.py
@@ -0,0 +1,29 @@
+from django_filters import rest_framework as filters
+from rest_framework import filters as drf_filters
+from articles.models import Article
+
+
+class ArticleFilter(filters.FilterSet):
+    date = filters.DateFromToRangeFilter("date_published", label="before")
+    source = filters.CharFilter("source", lookup_expr="icontains")
+
+    class Meta:
+        model = Article
+        fields = ["source"]
+
+
+class FieldsOnlySearchFilter(drf_filters.SearchFilter):
+    def get_search_fields(self, view, request):
+        fields = []
+        if request.query_params.get("title_only"):
+            fields.append("title")
+        elif request.query_params.get("description_only"):
+            fields.append("description")
+        else:
+            fields = super().get_search_fields(view, request)
+
+        if request.query_params.get("regex"):
+            fields = [f"${field}" for field in fields]
+            print(fields)
+
+        return fields
--- backend/src/articles/api/views.py
+++ backend/src/articles/api/views.py
@@ -3,45 +3,33 @@ from rest_framework import generics
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
+from django_filters import rest_framework as df_filters
+from rest_framework import filters as rest_filters
 from articles.api.pagination import ArticlePagination
 from articles.api.serializers import ArticleSerializer
+from articles.api.filters import ArticleFilter, FieldsOnlySearchFilter
 from articles.models import Article
 
 
 class ArticleListView(generics.ListAPIView):
+    queryset = Article.objects.all()
     permission_classes = (IsAuthenticated,)
     serializer_class = ArticleSerializer
     pagination_class = ArticlePagination
-
-    def get_queryset(self):
-        qs = Article.objects.all()
-
-        # filtering by url params
-        source_param = self.request.query_params.get("source")
-        if source_param:
-            qs = qs.filter(source__icontains=source_param)
-
-        title_param = self.request.query_params.get("title")
-        if title_param:
-            qs = qs.filter(title__icontains=title_param)
-
-        date_from, date_to = (
-            self.request.query_params.get("from"),
-            self.request.query_params.get("to"),
-        )
-        if date_from:
-            date_from = dateutil.parser.parse(date_from)
-            qs = qs.filter(date_published__gte=date_from)
-        if date_to:
-            date_to = dateutil.parser.parse(date_to)
-            qs = qs.filter(date_published__lte=date_to)
-
-        return qs
+    filter_backends = (df_filters.DjangoFilterBackend, FieldsOnlySearchFilter)
+    filter_class = ArticleFilter
+    # "By default, searches will use case-insensitive partial matches."
+    search_class = FieldsOnlySearchFilter
+    search_fields = ["title", "description"]
 
 
 class ListAllSourcesView(APIView):
     permission_classes = (IsAuthenticated,)
 
     def get(self, request, format=None):
+        # for some reason, .values("source").distinct() returns ~10k values,
+        # even though there's only 3 sources (at time of writing).
+        # (I assume this has to do with "source" being a `models.URLField`)
+        # Hence the "ugly" set-casting
         sources = set([s["source"] for s in Article.objects.values("source").distinct()])
         return Response(sources)
