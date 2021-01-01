c73bf5447a8f9e070425ece5ae5aa2da8f0ba5a5
JonasRoo <jonas-roosen@t-online.de>
2021-01-01 23:08:42 +0100

    rename pageSize arg

--- backend/src/articles/api/pagination.py
+++ backend/src/articles/api/pagination.py
@@ -3,5 +3,5 @@ from rest_framework.pagination import PageNumberPagination
 
 class ArticlePagination(PageNumberPagination):
     page_size = 100
-    page_size_query_param = "page_size"
+    page_size_query_param = "pageSize"
     max_page_size = 500
