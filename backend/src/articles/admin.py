78af073a9d7a2ec261ec9b799dde40baa2afd437
JonasRoo <jonas-roosen@t-online.de>
2021-01-06 18:22:11 +0100

    register new models

--- backend/src/articles/admin.py
+++ backend/src/articles/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Article
+from .models import Article, Source, Heart
 
 admin.site.register(Article)
+admin.site.register(Source)
+admin.site.register(Heart)
