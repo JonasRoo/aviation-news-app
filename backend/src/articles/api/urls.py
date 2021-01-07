ddafdb65a072e5c9a308598d2ec90487185cab86
JonasRoo <jonas-roosen@t-online.de>
2021-01-07 14:11:36 +0100

    add caching

--- backend/src/articles/api/urls.py
+++ backend/src/articles/api/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page, cache_control
 from articles.api.views import (
     ArticleListView,
     SourceListView,
@@ -10,9 +11,18 @@ from articles.api.views import (
 
 app_name = "articles-api"
 
+
+def cache_private(func, *cache_page_args, **cache_kwargs):
+    return cache_page(cache_page_args, **cache_kwargs)(cache_control(private=True)(func))
+
+
 urlpatterns = [
-    path("list/", ArticleListView.as_view(), name="get-list-of-articles"),
-    path("list/", ArticleListView.as_view(), name="get-list-of-articles"),
+    path(
+        "list/",
+        # only using 10 seoncs here because of the "heart" endpoint
+        ArticleListView.as_view(),
+        name="get-list-of-all-articles-without-tags",
+    ),
     path(
         "list/hearted/",
         OnlyHeartedArticlesListView.as_view(),
@@ -21,8 +31,16 @@ urlpatterns = [
     path(
         "list/notTagged/",
         ArticleForTaggingListView.as_view(),
-        name="get-list-of-articles-user-has-not-tagget",
+        name="get-list-of-articles-user-has-not-tagged",
     ),
-    path("sources/", SourceListView.as_view(), name="get-list-of-all-possible-sources"),
-    path("<int:article_id>/heart/", HeartView.as_view(), name="get-list-of-all-possible-sources"),
+    path(
+        "sources/",
+        cache_private(SourceListView.as_view(), 60 * 60),
+        name="get-list-of-all-possible-sources",
+    ),
+    path(
+        "<int:article_id>/heart/",
+        HeartView.as_view(),
+        name="get-list-of-all-possible-sources",
+    ),
 ]
